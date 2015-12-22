a77cc298b983bfb9bc07fe461591b13bc314059c
wendi <ifyouseewendy@gmail.com>
2015-12-22 23:00:03 +0800

    client: Add headers

--- lib/stock_fighter/client.rb
+++ lib/stock_fighter/client.rb
@@ -5,13 +5,6 @@ module StockFighter
     include HTTParty
     base_uri 'https://api.stockfighter.io/ob/api'
 
-    CONFIG = Struct.new(:apikey, :account, :venue) do
-      def validate!
-        key = %i(apikey account venue).detect{|k| self.public_send(k).nil? }
-        raise ArgumentError, "#{key} should not be blank" unless key.nil?
-      end
-    end
-
     class Parser::Simple < HTTParty::Parser
       def parse
         JSON.parse(body).with_indifferent_access
@@ -20,14 +13,30 @@ module StockFighter
 
     parser Parser::Simple
 
+    CONFIG = Struct.new(:apikey, :account, :venue) do
+      def validate!
+        key = %i(apikey account venue).detect{|k| self.public_send(k).nil? }
+        raise ArgumentError, "#{key} should not be blank" unless key.nil?
+      end
+    end
+
+    TYPES = [:limit, :market, :fill_or_kill, :immediate_or_cancel]
+
     def initialize
       yield (@config=CONFIG.new)
-      @config.validate!
+
+      config.validate!
     end
 
     def check_api_status
-      self.class.get('/heartbeat')
+      self.class.get('/heartbeat', headers)
     end
+
+    private
+
+      def headers
+        @_headers ||= { headers: { "X-Stockfighter-Authorization" => config.apikey } }
+      end
 
   end
 end
