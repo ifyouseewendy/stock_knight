ee7e2227181e5b4020397314b3da94be5544c2fb
wendi <ifyouseewendy@gmail.com>
2015-12-22 22:22:26 +0800

    httparty: Config and parse body as HWIA*
    HashWithIndifferentAccess

--- lib/stock_fighter/client.rb
+++ lib/stock_fighter/client.rb
@@ -2,6 +2,9 @@ module StockFighter
   class Client
     attr_reader :config
 
+    include HTTParty
+    base_uri 'https://api.stockfighter.io/ob/api'
+
     CONFIG = Struct.new(:apikey, :account, :venue) do
       def validate!
         key = %i(apikey account venue).detect{|k| self.public_send(k).nil? }
@@ -9,9 +12,18 @@ module StockFighter
       end
     end
 
+    class Parser::Simple < HTTParty::Parser
+      def parse
+        JSON.parse(body).with_indifferent_access
+      end
+    end
+
+    parser Parser::Simple
+
     def initialize
       yield (@config=CONFIG.new)
       @config.validate!
     end
+
   end
 end
